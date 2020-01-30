c48822be930d4772ef9ee303d593869ac016bbfd
Abner Alcântara <abneralcantaradev@gmail.com>
2020-01-30 16:21:35 -0300

    Script modificado (adicionado barra de progresso)

--- bin/script.sh
+++ bin/script.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
-echo qual o diretorio?
+echo "Qual o diretório?"
 
 read DIRECTORY
 
-echo $(find /home/$(whoami)/$DIRECTORY/ -type d -exec chmod 755 {} \; && find /home/$(whoami)/$DIRECTORY/ -type f -exec chmod 644 {} \; && find /home/$(whoami)/$DIRECTORY/ -name wkhtmltopdf* -exec chmod 755 {} \; && find /home/$(whoami)/$DIRECTORY/ -name phantomjs* -exec chmod 755 {} \; && find /home/$(whoami)/$DIRECTORY/ -name zend* -exec chmod 755 {} \; && find /home/$(whoami)/$DIRECTORY/ -name 7za* -exec chmod 755 {} \; && find /home/$(whoami)/$DIRECTORY/ -name qpdf-linux-amd64* -exec chmod 755 {} \;)
+echo "[=>                           ] 0%"
+echo $(find /home/$(whoami)/$DIRECTORY/ -type d -exec chmod 755 {} \;)
+echo "[====>                        ] 15%"
+echo $(find /home/$(whoami)/$DIRECTORY/ -type f -exec chmod 644 {} \;)
+echo "[========>                    ] 30%"
+echo $(find /home/$(whoami)/$DIRECTORY/ -name wkhtmltopdf* -exec chmod 755 {} \;)
+echo "[============>                ] 45%"
+echo $(find /home/$(whoami)/$DIRECTORY/ -name phantomjs* -exec chmod 755 {} \;)
+echo "[================>            ] 60%"
+echo $(find /home/$(whoami)/$DIRECTORY/ -name zend* -exec chmod 755 {} \;)
+echo "[====================>        ] 75%"
+echo $(find /home/$(whoami)/$DIRECTORY/ -name 7za* -exec chmod 755 {} \;)
+echo "[========================>    ] 90%"
+echo $(find /home/$(whoami)/$DIRECTORY/ -name qpdf-linux-amd64* -exec chmod 755 {} \;)
+echo "[============================>] 100%"
+echo "Permissões corrigidas com sucesso!"
