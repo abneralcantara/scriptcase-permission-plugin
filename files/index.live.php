3c783f593959d95d029d5f6d8a5c20ce4759590c
Abner Alcântara <abneralcantaradev@gmail.com>
2020-01-31 17:18:05 -0300

    Some modifications

--- files/index.live.php
+++ files/index.live.php
@@ -1,3 +1,26 @@
+<html>
+<body>
+    <form name="form" action="" method="get">
+        <input type="text" name="user" id="user">
+        </br>
+    </br>
+	<input type="text" name="directory" id="directory">
+    </br></br>
+    <button type="button">Aplicar</button>
+    </form>
+
 <?php
-    //Arquivo principal que conterá sua rotina
+    $_GET['user'];
+    $_GET['directory'];
+    
+    if ($user != 0 || $user < 3) {
+        echo "Inválido";
+    }
+    else {
+        if ($directory <= 0) {
+            $directory = "public_html";
+        }
+    }
 ?>
+</body>
+</html>
