76afe3e5537824abc110f17f790bf68b14c26949
Abner Alcântara <abneralcantaradev@gmail.com>
2020-02-05 16:45:39 -0300

    Adding some of the functions

--- files/index.live.php
+++ files/index.live.php
@@ -1,26 +1,11 @@
-<html>
-<body>
-    <form name="form" action="" method="get">
-        <input type="text" name="user" id="user">
-        </br>
-    </br>
-	<input type="text" name="directory" id="directory">
-    </br></br>
-    <button type="button">Aplicar</button>
-    </form>
-
 <?php
-    $_GET['user'];
-    $_GET['directory'];
-    
-    if ($user != 0 || $user < 3) {
-        echo "Inválido";
-    }
-    else {
-        if ($directory <= 0) {
-            $directory = "public_html";
-        }
-    }
+$caminho = shell_exec('pwd');
+
+if (substr($caminho, 1, 4) == "home") {
+	if (trim(substr($caminho, 6, -1)) == trim(shell_exec('whoami'))) {
+		echo "sucesso\n";
+	} else {
+		echo "falhou\n";
+	};
+};
 ?>
-</body>
-</html>
