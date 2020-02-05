a8b9ce346612afb0c038733ba8d1e319e8c31d72
Abner Alcântara <abneralcantaradev@gmail.com>
2020-02-05 16:45:12 -0300

    Adding some of the functions

--- files/index.php
+++ files/index.php
@@ -1,3 +1,11 @@
-<?php
-    //Irá redirecionar para o arquivo index.live.php
-?>
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<form action="index.live.php" method="post">
+Informe aqui o diretório a ser corrigido: <input type="text" name="diretorio"><br>
+<input type="submit">
+</form>
+</body>
+</html>
