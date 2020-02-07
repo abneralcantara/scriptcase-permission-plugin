29a58af7660c4adef8345db4b455de14e7e4ed6f
Abner Alcântara <abneralcantaradev@gmail.com>
2020-02-07 16:00:10 -0300

    Redirection added

--- files/index.php
+++ files/index.php
@@ -1,11 +1,15 @@
+<?php
+	$directory = trim(shell_exec('pwd'))."/";
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
+	<title>Teste</title>
 </head>
 <body>
-<form action="index.live.php" method="post">
-Informe aqui o diretório a ser corrigido: <input type="text" name="diretorio"><br>
-<input type="submit">
-</form>
+	<form action="index.live.php" method="post">
+	Informe aqui o diretório a ser corrigido: <input type="text" name="directory_name"><br>
+	<input type="submit">
 </body>
 </html>
