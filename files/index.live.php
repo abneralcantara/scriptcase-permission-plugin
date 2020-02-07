80b4865f0eb41f5d7835d938c66b5dbc6995f860
Abner Alcântara <abneralcantaradev@gmail.com>
2020-02-07 16:01:02 -0300

    Adding security methods

--- files/index.live.php
+++ files/index.live.php
@@ -1,11 +1,17 @@
 <?php
-$caminho = shell_exec('pwd');
+$directory = trim(shell_exec('pwd'))."/";
+$directory_name = htmlspecialchars($_POST['directory_name']);
+$complete_directory = trim($directory).trim($directory_name);
 
-if (substr($caminho, 1, 4) == "home") {
-	if (trim(substr($caminho, 6, -1)) == trim(shell_exec('whoami'))) {
+//Verifying if the user have the permitions to change anything.
+
+if (substr($complete_directory, 1, 4) == "home") {
+	if (trim(substr($complete_directory, 6, -1)) == trim(shell_exec('whoami'))) {
 		echo "sucesso\n";
+		//Verify if are special Chars.
 	} else {
-		echo "falhou\n";
+		echo "Você não tem permissão para realizar alterações neste diretório.\n";
 	};
 };
+
 ?>
