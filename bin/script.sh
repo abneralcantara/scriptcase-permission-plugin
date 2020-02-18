1463410142f433133564b310c5ea3d0ebf8ea202
Abner Alcântara <abneralcantaradev@gmail.com>
2020-02-18 00:54:57 -0300

    Final Script

--- bin/script.sh
+++ bin/script.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
 
-DIRECTORY= $(cat path_d.txt)
-
 echo "[=>                           ] 0%"
-echo $(find /home/$(whoami)/$DIRECTORY/ -type d -exec chmod 755 {} \;)
+
+find /home/$(whoami)/$total_directory/ -type d -exec chmod 755 {} \;
+
 echo "[====>                        ] 15%"
-echo $(find /home/$(whoami)/$DIRECTORY/ -type f -exec chmod 644 {} \;)
+
+find /home/$(whoami)/$total_directory/ -type f -exec chmod 644 {} \;
+
 echo "[========>                    ] 30%"
-echo $(find /home/$(whoami)/$DIRECTORY/ -name wkhtmltopdf* -exec chmod 755 {} \;)
+
+find /home/$(whoami)/$total_directory/ -name wkhtmltopdf* -exec chmod 755 {} \;
+
 echo "[============>                ] 45%"
-echo $(find /home/$(whoami)/$DIRECTORY/ -name phantomjs* -exec chmod 755 {} \;)
+
+find /home/$(whoami)/$total_directory/ -name phantomjs* -exec chmod 755 {} \;
+
 echo "[================>            ] 60%"
-echo $(find /home/$(whoami)/$DIRECTORY/ -name zend* -exec chmod 755 {} \;)
+
+find /home/$(whoami)/$total_directory/ -name zend* -exec chmod 755 {} \;
+
 echo "[====================>        ] 75%"
-echo $(find /home/$(whoami)/$DIRECTORY/ -name 7za* -exec chmod 755 {} \;)
+
+find /home/$(whoami)/$total_directory/ -name 7za* -exec chmod 755 {} \;
+
 echo "[========================>    ] 90%"
-echo $(find /home/$(whoami)/$DIRECTORY/ -name qpdf-linux-amd64* -exec chmod 755 {} \;)
+
+find /home/$(whoami)/$total_directory/ -name qpdf-linux-amd64* -exec chmod 755 {} \;
+
 echo "[============================>] 100%"
